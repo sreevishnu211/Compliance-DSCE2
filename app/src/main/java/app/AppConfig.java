3d77c940462e8a81a8db9466e8dcc06246a1cb28
swapnil1104 <swapniltiwari775@gmail.com>
2017-03-17 01:28:26 +0530

    Trying Git commit, added button on the MainActivity 2 components Login/Register Functionalities added along with session manager and app controller.

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by Swapnil on 17-03-2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://affbiz.co/compliance/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://affbiz.co/compliance/register.php";
+}
--- app/src/main/java/cononymous/spark/com/compliance_dsce/MainActivity.java
+++ app/src/main/java/cononymous/spark/com/compliance_dsce/MainActivity.java
@@ -1,5 +1,6 @@
 package cononymous.spark.com.compliance_dsce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
         adapter = new RecyclerAdapterDept();
         recyclerView.setAdapter(adapter);
     }
+
 
 }
