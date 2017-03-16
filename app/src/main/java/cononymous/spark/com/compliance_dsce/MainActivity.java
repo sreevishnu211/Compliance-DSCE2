568ee71ff5d02a860c6caf019b570e4e931b0b87
swapnil1104 <swapniltiwari775@gmail.com>
2017-03-17 01:37:34 +0530

    Trying Git commit, added button on the MainActivity 2 components Login/Register Functionalities added along with session manager and app controller.

--- app/src/main/java/cononymous/spark/com/compliance_dsce/MainActivity.java
+++ app/src/main/java/cononymous/spark/com/compliance_dsce/MainActivity.java
@@ -6,21 +6,60 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import helper.SQLiteHandler;
+import helper.SessionManager;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.Adapter adapter;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new SQLiteHandler(getApplicationContext());
+        btnLogout = (Button) findViewById(R.id.logoutBtn);
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         adapter = new RecyclerAdapterDept();
         recyclerView.setAdapter(adapter);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+    }
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
