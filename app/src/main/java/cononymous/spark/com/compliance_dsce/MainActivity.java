f1704751817eb47aee846db8ca397a5d6bc32594
swapnil1104 <swapniltiwari775@gmail.com>
2017-03-17 01:37:54 +0530

    Trying Git commit, added button on the MainActivity 2 components Login/Register Functionalities added along with session manager and app controller.

--- app/src/main/java/cononymous/spark/com/compliance_dsce/MainActivity.java
+++ app/src/main/java/cononymous/spark/com/compliance_dsce/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 logoutUser();
+
             }
         });
 
